06a6089d65face0c98c025e403f4ddb527606567
Frank <fcimeson@gmail.com>
2017-05-18 08:37:27 -0400

    licence

--- include/main.hpp
+++ include/main.hpp
@@ -1,19 +1,19 @@
+/********************************************************************************
+  Copyright 2017 Frank Imeson and Stephen L. Smith
 
-/****************************************************************************//**
- ********************************************************************************
- * @file        subisosat.hpp
- * @brief       
- * @author      Frank Imeson
- * @date        
-    ToDos:
-      - implement LKH search for every solution (and first solution)
-      - limit number of added clauses
-      - limit number of added litterals
-      - time out?
-      - fix unsat bug
-      - fix batch loop
- ********************************************************************************
- ********************************************************************************/
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*********************************************************************************/
+
 
 #ifndef MAIN_H		// guard
 #define MAIN_H
